d4474111f9dc28ed31665ecd5e434f73838a8cf3
Eda Defterli <47542055+edadefterli@users.noreply.github.com>
2023-10-10 22:35:39 +0300

    Merge Sorted Array

---
+++ MergeSortedArray/src/Main.java
@@ -0,0 +1,15 @@
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) {
+        int[] nums1 = {1,2,3,0,0,0};
+        int m = 3;
+        int[] nums2 = {2,5,6};
+        int n = 3;
+        
+        Solution.merge(nums1,m,nums2,n);
+
+        System.out.println(Arrays.toString(nums1));
+
+    }
+}
